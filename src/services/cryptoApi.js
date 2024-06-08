7fddb18c59205894268f27c1a9ca5aeafcb07c3c
Satyum <satyumspathania@gmail.com>
2024-06-08 15:45:19 +0530

    working with cryptocurrencies.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,21 +1,25 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   "X-RapidAPI-Key": "3fb181ae5cmsh07eb1cad9e70b61p127d25jsn682c46ac743b",
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
-const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: "cryptoApi",
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints:(builder)=>({
-        getCryptos: builder.query({
-            query:()=>'/exchanges'
-        }),
-    })
-})
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
 
 // const options = {
 //   method: "GET",
