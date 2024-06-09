263e5a954686810cb75edf50bfbf0bf648084a89
Satyum <satyumspathania@gmail.com>
2024-06-09 16:09:48 +0530

    solved the fetching problem.

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,26 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-export const Cryptocurrencies = () => {
+export const Cryptocurrencies = ({ simplified }) => {
 
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+  const count = simplified ? 10 : 100;
+  const [searchTerm, setSearchTerm] = useState("");
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase())))
+  }, [cryptosList, searchTerm])
+
+  if (isFetching || !cryptosList) return <div>Loading...</div>;
+
+
 
   console.log(cryptos);
 
   return (
     <>
-      <Row gutters={[32,32]} className='crypto-card-container'>
-        {cryptos?.map((currency)=>(
+      {!simplified && (
+        <div className="search-crypto">
+          <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+        </div>
+      )}
+
+      <Row gutter={[32, 32]} className='crypto-card-container'>
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl}/>}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
                 hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
 
 export const News = () => {
   return (
