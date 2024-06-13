d3f62041c275ccaa3a18bff95fd81567cd4dc14c
Satyum <satyumspathania@gmail.com>
2024-06-13 12:56:08 +0530

    working on exchanges.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from "react-router-dom";
 import { HomeOutline, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined, HomeOutlined } from "@ant-design/icons";
 import icon from '../images/cryptocurrency.png';
 
 export const Navbar = () => {
-    const[activeMenu,setActiveMenu]=useState(true);
-    const[screenSize,setScreenSize]=useState(null);
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null);
 
-    useEffect(()=>{
-        const handleResize=()=>setScreenSize(window.innerWidth);
-        window.addEventListener('resize',handleResize);
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener('resize', handleResize);
         handleResize();
-        return ()=>window.removeEventListener('resize',handleResize);
-    },[])
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
 
-    useEffect(()=>{
-        if(screenSize<768) setActiveMenu(false);
+    useEffect(() => {
+        if (screenSize <= 800) setActiveMenu(false);
         else setActiveMenu(true);
-    },[screenSize])
+    }, [screenSize])
 
 
     return (
@@ -29,26 +29,27 @@ export const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">CryptoVerse</Link>
                 </Typography.Title>
-                {/* <Button className="menu-control-container">
+                <Button className="menu-control-container">
                     <MenuOutlined />
-                </Button> */}
+                </Button>
             </div>
-            {activeMenu && }
-
-            <Menu theme="dark" mode="inline">
-                <Menu.Item icon={<HomeOutlined />}>
-                    <Link to="/">Home</Link>
-                </Menu.Item>
-                <Menu.Item icon={<FundOutlined />}>
-                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                </Menu.Item>
-                <Menu.Item icon={<MoneyCollectOutlined />}>
-                    <Link to="/exchanges">Exchanges</Link>
-                </Menu.Item>
-                <Menu.Item icon={<BulbOutlined />}>
-                    <Link to="/news">News</Link>
-                </Menu.Item>
-            </Menu>
+            <Button className='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
+            {activeMenu && (
+                <Menu theme="dark" mode="inline">
+                    <Menu.Item icon={<HomeOutlined />}>
+                        <Link to="/">Home</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<FundOutlined />}>
+                        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<MoneyCollectOutlined />}>
+                        <Link to="/exchanges">Exchanges</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<BulbOutlined />}>
+                        <Link to="/news">News</Link>
+                    </Menu.Item>
+                </Menu>
+            )}
         </div>
     )
 }
